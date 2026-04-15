40b50af4f2e8451f0d0bf799d3ab8873df60e81f
AlfonsoauquillaSerrano <alfonsoauquilla180@gmail.com>
2026-04-15 09:04:19 -0500

    Ejemplos

--- src/App.java
+++ src/App.java
@@ -1,20 +1,44 @@
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int cantidadEstudiantes=60000000;
+        int cantitadConsultas=40_000;
 
-        int num =10;
-        int[] array =new int[]{1,2,3,4,5};
-        System.out.println(num);
-        System.out.println(array);
+        System.out.println("Generando estudiantes...");
+        //[] Est1,Est2,Est3,
+        List<Estudiante> estudiantes = Generador.generarEstudiantes(cantidadEstudiantes);
+            for(Estudiante est: estudiantes){
+                System.out.println(est.nombre);
+            }
 
-        for(int i=0;i<array.length;i++){
+        /// BUSCAR SI ESXISTE EL USUARIO
+        /// CON CI 1_999_993
+        String cedulaBuscar="51000000";
+        for(Estudiante est:estudiantes){
 
-            System.out.println(array[i]);
+            if (est.cedula.equals(cedulaBuscar)){
+                System.out.println("EXISTE");
+            }
         }
+        int inicio =0;
+        int fin = estudiantes.size()-1;
+        
+        // while (inicio<=fin) {
+        //     int medio=(inicio+fin)/2;
+        //     Estudiante actual = estudiantes.get(medio);
 
-        for(int i:array){
+        //     int comparacion = actual.cedula.compareTo(cedulaBuscar);
 
-            System.out.println(i);
-        }
+        //     if(comparacion == 0){  
+        //         System.out.println("EXISTE");
+        //         break;
+        //     }else if (comparacion < 0){
+        //         inicio=medio+1;
+        //     }else{
+        //         fin =medio-1;
+        //     }
+            
+        // }
     }
 }
